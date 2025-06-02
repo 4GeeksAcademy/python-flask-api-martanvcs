672b9657c51a67b4d8a3ab5e3bcaf5151aa24811
Grin <martanvcs@gmail.com>
2025-06-02 17:10:52 +0000

    primer commit

---
+++ src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask,jsonify,request
+
+
+app = Flask(__name__)
+
+todos =[
+   { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+   
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    try:
+       
+        todos.pop(position)     
+        return jsonify(todos), 200
+    except IndexError:
+        return jsonify({"error": "Invalid position"}), 400
+   
+   
+# Estas dos líneas siempre deben estar al final de tu archivo app.py
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
